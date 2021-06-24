7400e0ace6ee7dd90aa5ab42ea3cb880eee782e1
lucastrschneider <lucastrschneider@gmail.com>
2021-06-24 20:22:29 -0300

    📝 Update terminal msgs

--- EP1/__main__.py
+++ EP1/__main__.py
@@ -1,11 +1,13 @@
 from EP1 import tests
 
 def main():
-  print('#########################################')
-  print("Aplicação do Algoritmo QR")
-  print('#########################################\n')
-  print("Testes disponíveis:\n\tA - Descrição\n\tB - Descrição\n\tC - Descrição\n")
-  test_letter = input("Digite a letra correspondente ao teste que deve ser executado: ")
+  print('#############################################')
+  print('Aplicação do Algoritmo QR\n')
+  print('Jean Carlos Mello Xavier Faria - 11259628')
+  print('Lucas Tonini Rosenberg Schneider - 11260850')
+  print('#############################################\n')
+  print('Testes disponíveis:\n\tA - Descrição\n\tB - Descrição\n\tC - Descrição\n')
+  test_letter = input('Digite a letra correspondente ao teste que deve ser executado: ')
 
   if test_letter == 'a' or test_letter == 'A':
     print('\n\n\n')
