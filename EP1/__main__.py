646139841c923ff698ceac1fb416526eed2e3768
lucastrschneider <lucastrschneider@gmail.com>
2021-06-30 13:08:29 -0300

    📝 Add tests descriptions

--- EP1/__main__.py
+++ EP1/__main__.py
@@ -1,12 +1,22 @@
 from EP1 import tests
 
+HEADER = '''
+#############################################
+Aplicação do Algoritmo QR
+
+Jean Carlos Mello Xavier Faria - 11259628
+Lucas Tonini Rosenberg Schneider - 11260850
+#############################################
+'''
+
+DESC_A = 'Calculo de autovalores e autovetores para matriz A pre-definida'
+DESC_B = 'Evolução da solução de um sistema massa-mola com 5 massas'
+DESC_C = 'Evolução da solução de um sistema massa-mola com 10 massas'
+
+
 def main():
-  print('#############################################')
-  print('Aplicação do Algoritmo QR\n')
-  print('Jean Carlos Mello Xavier Faria - 11259628')
-  print('Lucas Tonini Rosenberg Schneider - 11260850')
-  print('#############################################\n')
-  print('Testes disponíveis:\n\tA - Descrição\n\tB - Descrição\n\tC - Descrição\n')
+  print(HEADER)
+  print(f'Testes disponíveis:\n\tA - {DESC_A}\n\tB - {DESC_B}\n\tC - {DESC_C}\n')
   test_letter = input('Digite a letra correspondente ao teste que deve ser executado: ')
 
   if test_letter == 'a' or test_letter == 'A':
