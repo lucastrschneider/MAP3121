1bbba4d1478ba77223c9f757c44016eab4cb29c2
lucastrschneider <lucastrschneider@gmail.com>
2021-07-08 12:24:08 -0300

    ✨ Finish first iteration
    Co-authored-by: JeanMX <JeanMX@users.noreply.github.com>

--- EP2/householder.py
+++ EP2/householder.py
@@ -10,11 +10,13 @@ class Householder:
 
     self.A = np.ndarray(A.shape, dtype=float)
 
+    self.Ht = np.identity(self.n, dtype=float)
+
     np.copyto(self.A, A)
 
 
   def _iterate_once(self):
-
+    # Calcula o valor do wk da iteracao atual
     ak_ = self.A[self.k+1 :, [self.k]]
 
     wk_ = np.copy(ak_)
@@ -34,15 +36,32 @@ class Householder:
     if self.print_steps:
       print(f'H_w{self.k+1} A:\n{self.A}')
 
-    
+    # Multiplica a matriz H_wk a direita de A
+    for i in range(self.k+1, self.n):
+      bi_ = self.A[[i], self.k+1 :]
+
+      self.A[[i], self.k+1 :] = bi_ - 2 * np.dot(bi_, wk_) / np.dot(wk_.T, wk_) * wk_.T
+
+    if self.print_steps:
+      print(f'H_w{self.k+1} A H_w{self.k+1}:\n{self.A}')
+
+    # Multiplica a matriz H_wk a direita de Ht
+    for i in range(self.k+1, self.n):
+      bi_ = self.Ht[[i], self.k+1 :]
+
+      self.Ht[[i], self.k+1 :] = bi_ - 2 * np.dot(bi_, wk_) / np.dot(wk_.T, wk_) * wk_.T
+
+    if self.print_steps:
+      print(f'Ht H_w{self.k+1}:\n{self.Ht}')
 
     self.k += 1
 
+
 if __name__ == '__main__':
   A = np.array([[2, -1, 1, 3],
-             [-1, 1, 4, 2],
-             [1, 4, 2, -1],
-             [3, 2, -1, 1]], dtype=float)
+                [-1, 1, 4, 2],
+                [1, 4, 2, -1],
+                [3, 2, -1, 1]], dtype=float)
 
   hh = Householder(A, print_steps=True)
 
