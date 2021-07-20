61af08ab16ec13bf517c2b0805f7d44c8eaf032b
JeanMello <jean.xavier@thunderatz.org>
2021-07-20 10:02:45 -0300

    🚸 Implement to read input file

--- EP2/test_a.py
+++ EP2/test_a.py
@@ -1,60 +1,48 @@
+import os
+
 import numpy as np
 from qr_method import QRMethod
 from householder import Householder
 
 EPSILON = 1e-6
-A1 = np.array([[2, 4, 1, 1],
-              [4, 2, 1, 1],
-              [1, 1, 1, 2],
-              [1, 1, 2, 1]], dtype=float)
 
-def fill_A_test_2(n):
-  A2 = np.ndarray((n, n), dtype=float)
 
+def fill_matrix(file_name):
+  f = open(file_name, 'r')
+  
+  #Tamanho da matriz 
+  n = int(f.readline())
+
+  #Matriz
+  A = np.ndarray((n, n), dtype=float)
+  
   for i in range(n):
-    for j in range(n):
-      if i <= j:
-        A2[i][j] = n - j
-      else:
-        A2[i][j] = n - i
-
-  return A2
+      line = f.readline().split()
+      for j in range(n):
+          A[j][i] = line[j]
+  
+  f.close()
+  
+  return A
 
 def run():
   print('#############################################')
   print('COMEÇANDO O TESTE A)')
   print('#############################################\n')
-
-  print('\nOpções disponíveis:')
-  print(f'1) A =\n{A1}\n')
-  print(f'2) A =')
-  print('[[ n  n-1 n-2 ...  2   1 ]')
-  print(' [n-1 n-1 n-2 ...  2   1 ]')
-  print(' [n-2 n-2 n-2 ...  2   1 ]')
-  print(' [ :   :   :  ...  :   : ]')
-  print(' [ 2   2   2   2   2   1 ]')
-  print(' [ 1   1   1   1   1   1 ]]\n')
-
-  try:
-    mode = int(input('Número da opção escolhida (entradas inválidas levam à opção 1): '))
-    if mode > 2 or mode <= 0:
-      raise ValueError
-  except:
-    mode = 1
-
-  if mode == 1:
-    A = A1
-  else:
+  
+  while True:
     try:
-      n = int(input('Digite o valor de n (padrão é 20): '))
-      if n <= 0:
-        raise ValueError
+      mode = input('Digite o nome do arquivo para o teste: ')
+
+      FILE = 'inputs/'+mode
+
+      script_dir = os.path.dirname(__file__) # caminho absoluto do script
+      A = fill_matrix(os.path.join(script_dir, FILE))
+      break
+
     except:
-      n = 20
+      print('\nNão existe este arquivo! Tente novamente...\n')
 
-    A = fill_A_test_2(n)
-
-  print('\n')
   print(f'A =\n{A}')
 
 
@@ -66,7 +54,7 @@ def run():
 
   eigen_values, eigen_vectors = method_qr.iterate(EPSILON)
 
-  # Checagem de cada autovalor com seu respectivo autovetor
+  # # Checagem de cada autovalor com seu respectivo autovetor
   eigen_errors = np.ndarray(eigen_values.shape, dtype=float)
 
   for i in range(eigen_values.shape[0]):
@@ -78,7 +66,7 @@ def run():
   
   print(f'\nMaximo de ||A.v - lambda.v|| = {np.max(eigen_errors)}')
 
-  # Checar se a matriz de autovetores e ortogonal
+  # # Checar se a matriz de autovetores e ortogonal
   identity_matrix = np.matmul(eigen_vectors.T,eigen_vectors)
   
   ortho_error  = np.linalg.norm(np.identity(eigen_vectors.shape[0]) - identity_matrix, ord=np.inf)
