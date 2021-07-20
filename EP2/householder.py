172b4efac7a6e610ad4bdf640f51c6c45fc4ee80
lucastrschneider <lucastrschneider@gmail.com>
2021-07-20 18:29:36 -0300

    📝 Add docstrings to householder

--- EP2/householder.py
+++ EP2/householder.py
@@ -1,6 +1,21 @@
 import numpy as np
 
 class Householder:
+  """
+  Classe que representa o algoritmo utilizado para fazer transformações de Householder
+  para reduzir uma matriz simétrica a uma tridiagonal simetrica semelhante.
+
+  Args:
+      A (numpy.ndarray): Array n por n que representa a matriz completa a ser reduzida.
+      print_steps (bool, optional): Indica se deve ou não imprimir os passos intermediários. Padrão é False.
+
+  Attributes:
+    n (int): Variável que indica a dimensão da matriz do sistema.
+    k (int): Guarda o número da iteração atual do algorítmo.
+    print_steps (bool): Indica se deve imprimir informações de cada iteração na tela.
+    A (numpy.ndarray): Guarda o valor atual da matriz A.
+    Ht (numpy.ndarray): Guarda o valor atual da matriz H transposta.
+  """
   def __init__(self, A, print_steps=False):
     assert A.shape[0] == A.shape[1]
 
@@ -15,6 +30,13 @@ class Householder:
     np.copyto(self.A, A)
 
   def _get_alfa_beta(self):
+    """
+    Retorna a diagonal pricipal e subdiagonal da matriz tridiagonalizada pelo algoritmo.
+
+    Returns:
+        [dict]: Dicionário com dois elementos, um numpy.ndarray representando a diagonal principal
+                cuja chave é "alfa", e um representando as subdiagonais, com chave "beta".
+    """
     alfa = np.ndarray(self.n, dtype=float)
     beta = np.ndarray(self.n-1, dtype=float)
 
@@ -32,6 +54,9 @@ class Householder:
     return result
 
   def _iterate_once(self):
+    """
+    Método que representa uma iteração de transformações de Householder para uma matriz simétrica.
+    """
     if self.print_steps:
       print(f'Iteração {self.k+1}:')
 
@@ -76,6 +101,13 @@ class Householder:
     self.k += 1
 
   def iterate(self):
+    """
+    Método que executa as transformações de Householder para a matriz fornecida no construtor.
+
+    Returns:
+        [dict]: Dicionário contendo a matriz tridiagonal e a matriz tranposta de H, para serem
+                utilizados pelo método QR.
+    """
     for m in range(self.n-2):
       self._iterate_once()
 
